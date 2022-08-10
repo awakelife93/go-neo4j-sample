cbacd996c3c6559708f82cd7009e21336bb51f0f
hw.park <hw.park@fastfive.co.kr>
2022-08-10 17:45:56 +0900

    query 호출 및 todo 추가

--- main.go
+++ main.go
@@ -7,8 +7,29 @@ import (
 )
 
 func start() {
-	error := neo4j.Initialize()
-	fmt.Println("Initialize Error ====>", error)
+	initializeResult, initializeError := neo4j.Initialize()
+
+	if initializeError != nil {
+		fmt.Println("Initialize Error ====>", initializeError.Error())
+		neo4j.ClearDriver()
+		neo4j.ClearSession()
+		return
+	}
+
+	fmt.Println(initializeResult)
+
+	queryResult, queryError := neo4j.Query(
+		"CREATE (a:Greeting) SET a.message = $message RETURN a.message + ', from node ' + id(a)",
+		map[string]interface{}{"message": "hello, world"},
+	)
+
+	// todo: TLS error: Remote end closed the connection, check that TLS is enabled on the server 트러블 슈팅
+	if queryError != nil {
+		fmt.Println("Query Error ====>", queryError.Error())
+		return
+	}
+
+	fmt.Println(queryResult)
 }
 
 func main() {
