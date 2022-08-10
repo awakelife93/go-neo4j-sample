7281d63f041389db1fa960aec65bb53c6dfbdf8b
hw.park <hw.park@fastfive.co.kr>
2022-08-10 17:45:32 +0900

    driver config 추가

--- neo4j/driver.go
+++ neo4j/driver.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/neo4j/neo4j-go-driver/neo4j"
 	"github.com/thoas/go-funk"
@@ -14,6 +15,11 @@ var sessionObject neo4j.Session = nil
 
 func connect() error {
 	uri := os.Getenv("uri")
+
+	if funk.IsEmpty(uri) {
+		uri = "bolt://localhost:7687"
+	}
+
 	username := os.Getenv("username")
 	password := os.Getenv("password")
 
@@ -21,7 +27,11 @@ func connect() error {
 		return errors.New("Empty Connect Info")
 	}
 
-	driver, error := neo4j.NewDriver(uri, neo4j.BasicAuth(username, password, ""))
+	driver, error := neo4j.NewDriver(uri, neo4j.BasicAuth(username, password, ""), func(config *neo4j.Config) {
+		config.MaxConnectionLifetime = 60 * 60 * time.Second
+		config.MaxConnectionPoolSize = 50
+		config.ConnectionAcquisitionTimeout = 2 * time.Minute
+	})
 
 	if error != nil {
 		return error
@@ -37,7 +47,7 @@ func setupSession() error {
 		return errors.New("Empty driverObject")
 	}
 
-	session, error := driverObject.NewSession(neo4j.SessionConfig{AccessMode: neo4j.AccessModeWrite})
+	session, error := driverObject.NewSession(neo4j.SessionConfig{AccessMode: neo4j.AccessModeWrite, DatabaseName: "neo4j"})
 
 	if error != nil {
 		return error
@@ -48,22 +58,22 @@ func setupSession() error {
 	return nil
 }
 
-func Initialize() error {
+func Initialize() (string, error) {
 	fmt.Println("Neo4j initialize")
 
 	connectError := connect()
 
 	if connectError != nil {
-		return connectError
+		return "", connectError
 	}
 
 	setupSessionError := setupSession()
 
 	if setupSessionError != nil {
-		return setupSessionError
+		return "", setupSessionError
 	}
 
-	return nil
+	return "initialize success", nil
 }
 
 func getSession() neo4j.Session {
@@ -73,7 +83,7 @@ func getSession() neo4j.Session {
 func ClearDriver() {
 	fmt.Println("Clear Neo4j Driver")
 
-	if !funk.IsEmpty(driverObject) {
+	if driverObject != nil {
 		driverObject.Close()
 		driverObject = nil
 	}
@@ -82,7 +92,7 @@ func ClearDriver() {
 func ClearSession() {
 	fmt.Println("Clear Neo4j Session")
 
-	if !funk.IsEmpty(sessionObject) {
+	if sessionObject != nil {
 		sessionObject.Close()
 		sessionObject = nil
 	}
